ba3c379332a4b9f2d91aaa92de6d36a836d0c113
juleenek <leeenek0@gmail.com>
2023-09-10 12:18:30 +0200

    router initialization

---
+++ src/pages/error/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Error: FC = () => {
+  return <h1>Oh no, something went wrong.</h1>;
+};
+
+export default Error;
---
+++ src/pages/homepage/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+const Homepage: FC = () => {
+    return <h1>Main Page</h1>;
+};
+
+export default Homepage;
