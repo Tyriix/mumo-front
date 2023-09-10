a63d8ac11c84c574220c725f36e49cdfd4b89216
juleenek <leeenek0@gmail.com>
2023-09-10 19:04:39 +0200

    Initial skeleton of offer cards

---
+++ src/components/features/offer/OfferCard/OfferCard.tsx
@@ -0,0 +1,24 @@
+import { FC } from 'react';
+import './offer-card.scss';
+
+interface Props {
+  title: string;
+  content: string[];
+  priceContent: string[];
+}
+
+const OfferCard: FC<Props> = ({ title, content, priceContent }) => {
+  return (
+    <div className='offer-card'>
+      <h3 className='offer-card__header'>{title}</h3>
+      {content.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+      {priceContent.map((paragraph, index) => (
+        <p key={index}>{paragraph}</p>
+      ))}
+    </div>
+  );
+};
+
+export default OfferCard;
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,7 +1,13 @@
 import { FC } from 'react';
+import HomeOfferSection from '../../components/features/home/HomeOfferSection/HomeOfferSection';
 
 const Homepage: FC = () => {
-  return <h1>Main Page</h1>;
+  return (
+    <>
+      <h1>Main Page</h1>
+      <HomeOfferSection />
+    </>
+  );
 };
 
 export default Homepage;
