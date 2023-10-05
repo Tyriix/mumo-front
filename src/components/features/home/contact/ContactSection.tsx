561d1cc8078b66060b6f2ff2ad51cd39d2ebd8bb
Tyriix <konrad.barszcz1@gmail.com>
2023-10-05 21:20:45 +0200

    Working contact component

--- src/components/features/home/contact/ContactSection.tsx
+++ src/components/features/home/contact/ContactSection.tsx
@@ -1,7 +1,12 @@
+import ContactForm, {FormData} from './contact-form/ContactForm'
 import ContactMap from './contact-map/ContactMap'
 import './contact-section.scss'
+import {FaFacebook, FaInstagram} from 'react-icons/fa'
 
 const ContactSection = () => {
+  function handleSubmit(data: FormData){
+    console.log(data);
+  }
   return (
     <>
       <div className="contact">
@@ -9,6 +14,19 @@ const ContactSection = () => {
         <div className="contact__container">
           <div className="contact__map">
             <ContactMap></ContactMap>
+          </div>
+          <div className='contact__form'>
+            <div className='contact__form-container'>
+            <div className='contact__icons'>
+            <FaFacebook />
+            <FaInstagram/>
+            </div>
+            <div className='contact__info'>
+              <span className='contact__info-phone'>(+48) 500-083-369</span>
+              <span className='contact__info-email'>szkolenie.mumo@gmail.com</span>
+            </div>
+            <ContactForm onSubmit={handleSubmit}></ContactForm>
+            </div> 
           </div>
         </div>
       </div>
