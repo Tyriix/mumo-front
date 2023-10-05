2c1205f208a4b799594b074e9e886ca42f6d88a5
Tyriix <konrad.barszcz1@gmail.com>
2023-10-05 19:10:48 +0200

    Working contact map

--- src/components/features/home/contact/ContactSection.tsx
+++ src/components/features/home/contact/ContactSection.tsx
@@ -1,9 +1,16 @@
+import ContactMap from './contact-map/ContactMap'
+import './contact-section.scss'
+
 const ContactSection = () => {
   return (
     <>
       <div className="contact">
         <h2 className="contact__header">KONTAKT</h2>
-        <div className="contact__container"></div>
+        <div className="contact__container">
+          <div className="contact__map">
+            <ContactMap></ContactMap>
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/components/features/home/contact/contact-map/ContactMap.tsx
@@ -0,0 +1,41 @@
+import { GoogleMap, Marker, useJsApiLoader } from '@react-google-maps/api'
+
+const containerStyle = {
+  width: '30em',
+  height: '40em',
+}
+const center = {
+  lat: 51.307016313473405,
+  lng: 17.067984857672194,
+}
+
+const ContactMap = () => {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: 'YOUR_API_KEY',
+  })
+
+  return (
+    <>
+      {isLoaded ? (
+        <>
+          <GoogleMap
+            mapContainerClassName="c-office-overview__map"
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+          >
+            <Marker
+              position={{
+                lat: center.lat,
+                lng: center.lng,
+              }}
+            ></Marker>
+          </GoogleMap>
+        </>
+      ) : null}
+    </>
+  )
+}
+
+export default ContactMap
