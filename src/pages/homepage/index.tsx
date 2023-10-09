833c7dd2a091ad13cdff03ab3713e7e74fb60fcc
Julia Linek <79119595+juleenek@users.noreply.github.com>
2023-10-09 22:01:19 +0200

    Revert "✨ MSP-13 - Homepage start view"

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,16 +1,13 @@
-import { FC, useRef } from 'react'
+import { FC } from 'react'
 import HomeOfferSection from '../../components/features/home/offer/OfferSection'
 import AboutSection from '../../components/features/home/about/AboutSection'
-import StartViewSection from '../../components/features/home/start-view/StartViewSection'
 
 const Homepage: FC = () => {
-  const refOffer = useRef<HTMLDivElement>(null)
-
   return (
     <>
-      <StartViewSection refOffer={refOffer} />
+      <h1>Main Page</h1>
       <AboutSection />
-      <HomeOfferSection ref={refOffer} />
+      <HomeOfferSection />
     </>
   )
 }
