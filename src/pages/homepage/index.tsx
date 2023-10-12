66f0b8854094e0f334db4603f9ecde2e41b02d30
juleenek <leeenek0@gmail.com>
2023-10-12 14:39:19 +0200

    :lipstick: Added temporary ghost element for hompage

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,20 +1,27 @@
-import { FC, useRef } from 'react'
-import HomeOfferSection from '../../layout/homepage/offer/OfferSection'
-import AboutSection from '../../layout/homepage/about/AboutSection'
-import StartViewSection from '../../layout/homepage/start-view/StartViewSection'
-import ContactSection from '../../layout/homepage/contact/ContactSection'
+import { FC, useRef } from 'react';
+import HomeOfferSection from '../../layout/homepage/offer/OfferSection';
+import AboutSection from '../../layout/homepage/about/AboutSection';
+import StartViewSection from '../../layout/homepage/start-view/StartViewSection';
+import ContactSection from '../../layout/homepage/contact/ContactSection';
 
 const Homepage: FC = () => {
-  const refOffer = useRef<HTMLDivElement>(null)
+  const refOffer = useRef<HTMLDivElement>(null);
+
+  const tempElement = {
+    color: 'white',
+    width: '100%',
+    height: '600px',
+  };
 
   return (
     <>
       <StartViewSection refOffer={refOffer} />
       <AboutSection />
       <HomeOfferSection ref={refOffer} />
+      <div style={tempElement}></div>
       <ContactSection />
     </>
-  )
-}
+  );
+};
 
 export default Homepage;
