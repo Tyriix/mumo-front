83e0afc6ca7dae543b648d00650d4cacd5063ebb
Tyriix <konrad.barszcz1@gmail.com>
2023-10-22 19:26:38 +0200

    :lipstick: Form styling changes

--- src/layout/register/register-form/RegisterForm.tsx
+++ src/layout/register/register-form/RegisterForm.tsx
@@ -30,134 +30,156 @@ const RegisterForm = () => {
         method="post"
       >
         <div className="register-form__container">
-          <div className="register-form__input-row">  
-              <label
-                className="register-form__label"
-                htmlFor="register-form__input-name"
-              >
-                Imię i nazwisko
-              <input
-                id="register-form__input-name"
-                type="text"
-                {...register("name")}
-                className={classnames(
-                  "register-form__input",
-                  errors.name && "register-form__input-alert-active"
-                )}
-              />
-              </label>
-            <div className="register-form__error-container">
-              <span className="register-form__error">
-                {errors.name?.message}
+          <div className="register-form__input-row">
+            <label
+              className="register-form__label"
+              htmlFor="register-form__input-name"
+            >
+              <span className="register-form__input-label-text">
+              Imię i nazwisko
               </span>
-            </div>
+
+              <div className="register-form__input-error-column">
+              <input
+                    id="register-form__input-name"
+                    type="text"
+                    {...register("name")}
+                    className={classnames(
+                      "register-form__input",
+                      errors.name && "register-form__input-alert-active"
+                    )}
+                  />
+                <div className="register-form__error-container">
+                  <span className="register-form__error">
+                    {errors.name?.message}
+                  </span>
+                </div>
+              </div>
+            </label>
           </div>
           <div className="register-form__input-row">
-              <label
-                className="register-form__label"
-                htmlFor="register-form__input-email"
-              >
-                Adres email
-              
-              <input
-                id="register-form__input-email"
-                type="email"
-                {...register("email")}
-                className="register-form__input"
-              />
-              </label>
-            <div className="register-form__error-container">
-              <span className="register-form__error">
-                {errors.email?.message}
+            <label
+              className="register-form__label"
+              htmlFor="register-form__input-email"
+            >
+              <span className="register-form__input-label-text">
+              Adres email
               </span>
-            </div>
+              <div className="register-form__input-error-column">
+                <input
+                  id="register-form__input-email"
+                  type="email"
+                  {...register("email")}
+                  className="register-form__input"
+                />
+                <div className="register-form__error-container">
+                  <span className="register-form__error">
+                    {errors.email?.message}
+                  </span>
+                </div>
+              </div>
+            </label>
           </div>
           <div className="register-form__input-row">
-              <label
-                className="register-form__label"
-                htmlFor="register-form__input-phone"
-              >
-                Numer telefonu
-              
-              <input
-                id="register-form__input-phone"
-                type="text"
-                {...register("phone")}
-                className="register-form__input"
-              />
-              </label>
-            <div className="register-form__error-container">
-              <span className="register-form__error">
-                {errors.phone?.message}
+            <label
+              className="register-form__label"
+              htmlFor="register-form__input-phone"
+            >
+              <span className="register-form__input-label-text">
+              Numer telefonu
               </span>
-            </div>
+              <div className="register-form__input-error-column">
+                <input
+                  id="register-form__input-phone"
+                  type="text"
+                  {...register("phone")}
+                  className="register-form__input"
+                />
+                <div className="register-form__error-container">
+                  <span className="register-form__error">
+                    {errors.phone?.message}
+                  </span>
+                </div>
+              </div>
+            </label>
           </div>
           <div className="register-form__input-row">
-              <label
-                className="register-form__label"
-                htmlFor="register-form__input-password"
-              >
-                Hasło
-              
-              <input
-                id="register-form__input-password"
-                type="password"
-                {...register("password")}
-                className="register-form__input"
-              />
-              </label>
-            <div className="register-form__error-container">
-              <span className="register-form__error">
-                {errors.password?.message}
+            <label
+              className="register-form__label"
+              htmlFor="register-form__input-password"
+            >
+              <span className="register-form__input-label-text">
+              Hasło
               </span>
-            </div>
+              <div className="register-form__input-error-column">
+                <input
+                  id="register-form__input-password"
+                  type="password"
+                  {...register("password")}
+                  className="register-form__input"
+                />
+                <div className="register-form__error-container">
+                  <span className="register-form__error">
+                    {errors.password?.message}
+                  </span>
+                </div>
+              </div>
+            </label>
           </div>
           <div className="register-form__input-row">
-              <label
-                className="register-form__label"
-                htmlFor="register-form__input-password-repeat"
-              >
-                Powtórz hasło
-              
-              <input
-                id="register-form__input-password-repeat"
-                type="password"
-                {...register("repeatPassword")}
-                className="register-form__input"
-              />
-              </label>
-            <div className="register-form__error-container">
-              <span className="register-form__error">
-                {errors.repeatPassword?.message}
+            <label
+              className="register-form__label"
+              htmlFor="register-form__input-password-repeat"
+            >
+              <span className="register-form__input-label-text">
+              Powtórz hasło
               </span>
-            </div>
+              <div className="register-form__input-error-column">
+                <input
+                  id="register-form__input-password-repeat"
+                  type="password"
+                  {...register("repeatPassword")}
+                  className="register-form__input"
+                />
+                <div className="register-form__error-container">
+                  <span className="register-form__error">
+                    {errors.repeatPassword?.message}
+                  </span>
+                </div>
+              </div>
+            </label>
           </div>
-          <div className="register-form__input-row">
-          <label className="register-form__checkbox-label">
-          
-          <span className="register-form__input-text"><input
-              type="checkbox"
-              className="register-form__input-checkbox"
-              {...register('agreeTerms')}
-            />Wyrażam zgodę na przetwarzanie moich danych osobowych w celu
-            udzielenia odpowiedzi na składaną wiadomość.</span>
-            
-          </label>
-          
-        <div className="register-form__error-container">
-              <span className="register-form__error">
-                {errors.agreeTerms?.message}
-              </span>
-            </div>
+          <div className="register-form__checkbox-row">
+            <label className="register-form__checkbox-label">
+              <div className="register-form__flex-helper"></div>
+              <div className='register-form__checkbox-wrapper'>
+              <div className="register-form__checkbox-input-text-row">
+                <input
+                  id="check"
+                  type="checkbox"
+                  className="register-form__input-checkbox"
+                  {...register("agreeTerms")}
+                />
+                <span className='register-form__input-checkbox-custom'></span>
+                <span className="register-form__checkbox-label-text">
+                  Wyrażam zgodę na przetwarzanie moich danych osobowych w celu
+                  udzielenia odpowiedzi na składaną wiadomość.
+                </span>
+              </div>
+              <div className="register-form__error-container">
+                <span className="register-form__error">
+                  {errors.agreeTerms?.message}
+                </span>
+              </div>
+              </div>
+            </label>
           </div>
-          
-          
         </div>
         <MainButton
-            type="submit"
-            className="register-form__submit-button"
-            content={"Zarejestruj się"}
-          />
+          type="submit"
+          className="register-form__submit-button"
+          content={"Zarejestruj się"}
+        />
       </Form>
     </>
   );
