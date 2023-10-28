e5e07171b72ad12d98de0f22661d33efc51e7a6b
juleenek <leeenek0@gmail.com>
2023-10-28 11:01:29 +0200

    :hammer: Scroll to top button fix

--- src/components/buttons/ScrollTopButton.tsx
+++ src/components/buttons/ScrollTopButton.tsx
@@ -1,13 +1,36 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { FaArrowCircleUp } from 'react-icons/fa';
 import { scrollToTop } from '../../utils/scrollUtils';
 import './button-components.scss';
+import classNames from 'classnames';
 
 const ScrollTopButton: FC = () => {
+  const [showButton, setShowButton] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 200) {
+      setShowButton(true);
+    } else {
+      setShowButton(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <button className='scroll-top-button' onClick={scrollToTop}>
-      <FaArrowCircleUp className='scroll-top-button__icon' aria-label='Scroll to top button' />
+    <button
+      className={classNames('scroll-top-button', showButton && 'show-button')}
+      onClick={scrollToTop}
+    >
+      <FaArrowCircleUp
+        className='scroll-top-button__icon'
+        aria-label='Scroll to top button'
+      />
     </button>
   );
 };
