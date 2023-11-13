183c16ac674230ff83c261c46fe0170a52781e76
Tyriix <konrad.barszcz1@gmail.com>
2023-11-13 21:34:33 +0100

    :hammer: Swiper changes

--- src/layout/homepage/clients/ClientsSection.tsx
+++ src/layout/homepage/clients/ClientsSection.tsx
@@ -35,7 +35,7 @@ const ClientSection: FC = () => {
         }
           }
           navigation={{ nextEl: '.clients__swiper-button-right', prevEl: '.clients__swiper-button-left' }}
-          modules={[ Navigation]}
+          modules={[Autoplay, Navigation]}
           >
             <SwiperSlide>
               <ClientCard
