8cd0b14c8a39a3b11ee3f9374f4e17f6ab025973
juleenek <leeenek0@gmail.com>
2023-11-14 20:50:48 +0100

    :sparkles: Logotype navigate to homepage

--- src/layout/header/logotype/Logotype.tsx
+++ src/layout/header/logotype/Logotype.tsx
@@ -1,52 +1,81 @@
 import { FC } from 'react';
 import './logotype.scss';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
+import {
+  onNavbarAsyncClick,
+  onNavbarLinkClick,
+} from '../../../utils/navigateUtils';
+import { HomepageSections } from '../../../models/enums.app';
 
 interface Props {
   className?: string;
   isFirstNameVisible?: boolean;
   isSecondNameVisible?: boolean;
+  toggleNavbar?: () => void;
 }
 
 const LOGO_SRC = '/src/assets/images/logo-bg-01.png';
 
 const Logotype: FC<Props> = ({
   className,
+  toggleNavbar,
   isFirstNameVisible = true,
   isSecondNameVisible = true,
 }) => {
+  const navigate = useNavigate();
+  const isHomePage = location.pathname != '/';
+
+  const navigateNotHome = async () => {
+    const res = navigate('/');
+    return res;
+  };
+
   return (
-    <div className={classNames('logotype', className)}>
-      <img
-        className='logotype__img'
-        src={LOGO_SRC}
-        alt='Logo Mumo pozytywne szkolenie psów'
-        draggable='false'
-      />
-      <div
-        className={classNames(
-          'logotype__name',
-          !isFirstNameVisible && 'invisible'
-        )}
-      >
-        <p
+    <a
+      onClick={() =>
+        isHomePage
+          ? onNavbarAsyncClick(
+              HomepageSections.Home,
+              navigateNotHome,
+              toggleNavbar
+            )
+          : onNavbarLinkClick(HomepageSections.Home, toggleNavbar)
+      }
+      tabIndex={0}
+    >
+      <div className={classNames('logotype', className)}>
+        <img
+          className='logotype__img'
+          src={LOGO_SRC}
+          alt='Logo Mumo pozytywne szkolenie psów'
+          draggable='false'
+        />
+        <div
           className={classNames(
-            'logotype__name-child',
+            'logotype__name',
             !isFirstNameVisible && 'invisible'
           )}
         >
-          Mumo
-        </p>
-        <p
-          className={classNames(
-            'logotype__name-child',
-            !isSecondNameVisible && 'invisible'
-          )}
-        >
-          pozytywne szkolenie psów
-        </p>
+          <p
+            className={classNames(
+              'logotype__name-child',
+              !isFirstNameVisible && 'invisible'
+            )}
+          >
+            Mumo
+          </p>
+          <p
+            className={classNames(
+              'logotype__name-child',
+              !isSecondNameVisible && 'invisible'
+            )}
+          >
+            pozytywne szkolenie psów
+          </p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
