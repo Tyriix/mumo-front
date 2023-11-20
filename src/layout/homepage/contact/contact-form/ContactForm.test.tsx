3bfa599f334f7f1555bdc68052719fa3165e064b
Tyriix <konrad.barszcz1@gmail.com>
2023-11-20 23:04:06 +0100

    :rotating_light: Added render test

--- src/layout/homepage/contact/contact-form/ContactForm.test.tsx
+++ src/layout/homepage/contact/contact-form/ContactForm.test.tsx
@@ -1,9 +1,24 @@
-import { describe, it } from 'vitest';
-import { renderWithProviders } from '../../../../__tests__/utils/_test-utils';
+import { describe, expect, it } from 'vitest';
+import { screen } from '@testing-library/react'
+import { renderWithProviders } from '../../../../utils/testUtils';
 import ContactForm from './ContactForm';
 
 describe('Contact', async () => {
-  it('renders without errors', async () => {
+  it('Should render correctly', async () => {
     renderWithProviders(<ContactForm />);
+    const emailLabel = screen.getByLabelText('Email');
+    expect(emailLabel).toBeInTheDocument();
+
+    const nameLabel = screen.getByLabelText('Imię');
+    expect(nameLabel).toBeInTheDocument();
+
+    const messageLabel = screen.getByLabelText('Wiadomość');
+    expect(messageLabel).toBeInTheDocument();
+
+    const agreeTermsLabel = screen.getByLabelText('Wyrażam zgodę na przetwarzanie moich danych osobowych w celu udzielenia odpowiedzi na składaną wiadomość.');
+    expect(agreeTermsLabel).toBeInTheDocument();
+
+    const submitButton = screen.getByText('Wyślij');
+    expect(submitButton).toBeInTheDocument();
   });
 });
