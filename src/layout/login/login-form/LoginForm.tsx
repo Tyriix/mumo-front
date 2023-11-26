e7a639852510f55b853d0698247d0092af831a1a
Tyriix <konrad.barszcz1@gmail.com>
2023-11-26 15:35:51 +0100

    :hammer: Working on login

--- src/layout/login/login-form/LoginForm.tsx
+++ src/layout/login/login-form/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { Form, useNavigate } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
+import { FieldErrors, useForm } from 'react-hook-form';
 import './login-form.scss';
 import * as yup from 'yup';
 import MainButton from '../../../components/buttons/MainButton';
@@ -8,8 +8,20 @@ import { FcGoogle } from 'react-icons/fc';
 import { loginFormSchema } from '../../../models/schemas.yup';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useLoginUserMutation } from '../../../api/auth/authApi';
+import { useEffect, useState } from 'react';
 
+const WRONG_EMAIL_OR_PASSWORD = 'Użytkownik o takim adresie email nie istnieje. Spróbuj ponownie';
 export type LoginSchemaType = yup.InferType<typeof loginFormSchema>;
+const useClearUserExistErrorEffect = (
+  errors: FieldErrors<LoginSchemaType>,
+  setWrongEmailOrPassword: React.Dispatch<React.SetStateAction<boolean>>
+) => {
+  useEffect(() => {
+    if (errors.email?.message) {
+      setWrongEmailOrPassword(false);
+    }
+  }, [errors.email?.message, setWrongEmailOrPassword]);
+};
 
 const LoginForm = () => {
   const {
@@ -19,15 +31,32 @@ const LoginForm = () => {
   } = useForm<LoginSchemaType>({
     resolver: yupResolver(loginFormSchema),
   });
-  const [loginUser] = useLoginUserMutation();
+ 
   const navigate = useNavigate();
+  const [isEmailWrongOrPassword, setWrongEmailOrPassword] = useState(false);
+  const [isLoginError, setIsLoginError] = useState(false);
+  const [loginUser] = useLoginUserMutation();
+  useClearUserExistErrorEffect(errors, setWrongEmailOrPassword);
 
   const onSubmit = async (data: LoginSchemaType) => {
     try {
-        await loginUser(data);
-        navigate('/');
+      const response = await loginUser(data);
+      if ('error' in response) {
+        const error = response.error as Error;
+        if ('data' in error) {
+          if (error.data === WRONG_EMAIL_OR_PASSWORD) {
+            setWrongEmailOrPassword(true);
+            setIsLoginError(false);
+          } else {
+            setWrongEmailOrPassword(false);
+            setIsLoginError(true);
+          }
+        }
+      } else {
+        navigate('/login');
+      }
     } catch (error) {
-      console.error('Error sending mail:', error);
+      console.error('Register error:', error);
     }
   };
 
@@ -50,6 +79,11 @@ const LoginForm = () => {
           <div className='login__form-error-container'>
             <span className='login__form-error'>{errors.email?.message}</span>
           </div>
+          {isEmailWrongOrPassword && (
+                    <span className='register-form__error'>
+                      Uzytkownik o tym mailu juz istnieje.
+                    </span>
+                  )}
           <input
             id='login__-form-input-password'
             type='password'
@@ -77,6 +111,11 @@ const LoginForm = () => {
               />
               <FcGoogle aria-label='Google' className='login__form-icon' />
             </div>
+            {isLoginError && (
+            <span className='register-form__error'>
+              Wystąpił nieoczekiwany problem podczas procesu rejestracji.
+            </span>
+          )}
           </div>
         </div>
       </Form>
