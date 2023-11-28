1c6f357d391625f49d12e7b27c1242d57f58ba17
Tyriix <konrad.barszcz1@gmail.com>
2023-11-28 14:15:39 +0100

    :hammer: Working contact submit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import router from './app/router';
-import { store } from './app/store';
+import store from './app/store';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
